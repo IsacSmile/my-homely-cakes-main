371029c1ee1dfde8c20e3c9a85b878786d0fa60f
IsacSmile <94504780+IsacSmile@users.noreply.github.com>
2026-08-25 20:24:37 +0530

    Configure publish directory .next and serverExternalPackages in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ['better-sqlite3', '@libsql/client'],
   images: {
     unoptimized: true,
     remotePatterns: [
